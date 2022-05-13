dbc71c994ae1e9792cda5b9a775944f13289793c
EduardGeras <103679703+EduardGeras@users.noreply.github.com>
2022-05-13 20:21:11 +0500

    Домашка 1.9

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Author knyazev = new Author("Князев", "Милослав");
+        Book sets = new Book("Полный набор. Наследие древних", knyazev, 2016);
+        System.out.println("sets.getTitle = " + sets.getTitle());
+        System.out.println("knyazev.getSurname = " + knyazev.getSurname());
+        sets.setPublishingYear(2017);
+        System.out.println("sets.setPublishingYear = " + sets.getPublishingYear());
+
+        Author alyokhin = new Author("Алёхин", "Александр");
+        Book tom4 = new Book("Полное собрание партий с авторскими комментариями. Том 4. 1931—1946", alyokhin, 2017);
+        System.out.println("tom4.getTitle() = " + tom4.getTitle());
     }
 }
